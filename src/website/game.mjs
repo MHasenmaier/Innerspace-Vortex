172915d0fd31cff40060982c7ffbfaddb946858c
Marcel <marcel.hasenmaier@gmx.de>
2024-04-01 00:25:24 +0200

    added package.json and modified game.mjs for starting with basic functions

--- src/website/game.mjs
+++ src/website/game.mjs
@@ -1,3 +1,6 @@
+/**
+ * commented for debbuging reasons
+ * 
 import fs from 'fs';
 import path from 'path';
 
@@ -13,23 +16,86 @@ function loadJsonFile(filePath) {
 }
 
 // Pfade zur JSON-Datei anpassen
-const shipsJSON = path.join(__dirname, 'src', 'objects', 'ships.json');
-const playerJSON = path.join(__dirname, 'src', 'website', 'player.json');
+const shipsJSON = path.join('..', 'objects', 'ships.json');
+const playerJSON = path.join('..', 'website', 'player.json');
 
 // Lade die ShipsDaten aus der JSON-Datei
 const shipsDataJSON = loadJsonFile(shipsJSON);
 
 // Lade die PlayerDaten aus der JSON-Datei
 const playerDataJSON = loadJsonFile(playerJSON);
+*/
 
 // Zugriff auf das 4. Element (Index 3) in den Schiffdaten
+/*
 if (shipsDataJSON) {
     const ship = shipsDataJSON[3];
     console.log(`Das 4. Element in ships.json ist: ${JSON.stringify(ship)}`);
 } else {
     console.log('Fehler beim Laden der Schiffdaten aus der JSON-Datei.');
-}
+}*/
 
 // Spielerdaten ausgeben
-console.log('Spielerdaten:', playerDataJSON);
+//console.log('Spielerdaten:', playerDataJSON);
 
+let mine = {
+    "cost" : 100,
+    "prod" : 150,   //production per tick
+    "multP" : 4,    //production multiplier
+    "multC" : 4     //cost multiplier
+}
+
+let hq = {
+    "name": "hq",
+    "cost" : 200,
+}
+ 
+let player = {
+    buildings: {
+      hq: {
+        lvl: 1,
+      },
+      mine: {
+        lvl: 1,
+      }
+    },
+    ressourcen: {
+        "ress" : 200
+    }
+  };
+  
+console.log("player hq: " + player.buildings.hq.lvl);
+console.log("player mine: " + player.buildings.mine.lvl);
+console.log("player ress: " + player.ressourcen.ress);
+
+
+function payRess(buyObj, player)
+{
+    let ress = player.ressourcen.ress;
+    let cost = buyObj.cost;
+
+    if(ress - cost >= 0)
+    {
+        player.ressourcen.ress = player.ressourcen.ress - buyObj.cost;
+        return true;
+    }
+    console.log("!!!! payRess -> return false");
+    console.log("player.ressourcen.ress: " + player.ressourcen.ress);
+    console.log("buyObj.name: " + buyObj.cost + "; buyObj.cost: " + buyObj.cost);
+    return false;
+}
+
+function build(buyObj, player, buildObj)
+{
+    if (payRess(buyObj, player))
+    {
+        buildObj.lvl++;
+    }
+}
+
+build(hq, player, player.buildings.hq);
+
+console.log("---------- Nach Funktionsaufruf ----------");
+console.log("player hq: " + player.buildings.hq.lvl);
+console.log("player mine: " + player.buildings.mine.lvl);
+console.log("player ress: " + player.ressourcen.ress);
